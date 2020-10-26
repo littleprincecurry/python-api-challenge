c1654ba90f733d95dd47fb42357d192b0a77951e
littleprincecurry <scotwilson@gmail.com>
2020-10-25 21:58:43 -0400

    added data to dataframe from api

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "f891ccfb3f9ec5ff12c095cfe7ca373b"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "d64f5ea3deb9050b569a3fbc3b6b2fb0"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
